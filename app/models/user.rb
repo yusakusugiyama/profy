0bf3bd9f11b40dd18a85bea63e1c2d51eef7264c
Yusaku Sugiyama <yusakusugiyama@gmail.com>
2016-05-20 14:42:44 +0900

    implement group

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
     "#{family_name_kana} #{first_name_kana}"
   end
 
+  def full_profile?
+    avatar? && family_name? && first_name? && family_name_kana? && first_name_kana?
+  end
+
   #accessor
   attr_accessor :group_key
 
@@ -17,6 +21,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable,
          authentication_keys: [:email, :group_key]
+
+  #paperclip
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
+  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
+
 
   #association
   belongs_to :group
